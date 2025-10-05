28b44517e678e00d74c86453e4d437684b52c11b
Anugera Rejeki Abadi <martin12082000@gmail.com>
2025-10-05 21:05:50 +0700

    update 04

--- src/lib/verifySession.ts
+++ src/lib/verifySession.ts
@@ -5,7 +5,7 @@ export async function verifySession(req: NextRequest): Promise<boolean> {
   try {
     const cookie = req.headers.get('cookie') || ''
 
-    const res = await fetch(`${baseURL}/api/v1/auth/token`, {
+    const res = await fetch(`${baseURL}/auth/token`, {
       method: 'GET',
       headers: {
         cookie,
